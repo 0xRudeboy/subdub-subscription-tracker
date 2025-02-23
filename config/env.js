27f5b067f30f5f5a47e2f1a10b7a749e9c05e22c
0xRudeboy <rudeboyweb3@gmail.com>
2025-02-24 00:25:19 +0400

    add controllers, middlewares, routes and arcjet

--- config/env.js
+++ config/env.js
@@ -1,6 +1,13 @@
 import { config } from "dotenv";
-// import process from "process";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV } = process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_KEY,
+} = process.env;
---
+++ models/subscription.model.js
@@ -0,0 +1,105 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      requred: [true, "Subscription name is requred"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    price: {
+      type: Number,
+      required: [true, "Subscription price is required"],
+      min: [0, "Subscription price must be greater than 0"],
+    },
+    currency: {
+      type: String,
+      required: [true, "Subscription currency is required"],
+      enum: ["USD", "EUR", "GBP", "CAD", "AUD"],
+      default: "USD",
+    },
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly", "monthly", "yearly"],
+    },
+    category: {
+      type: String,
+      enum: [
+        "sports",
+        "finance",
+        "crypto",
+        "technology",
+        "science",
+        "health",
+        "entertainment",
+        "other",
+      ],
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "cancelled", "expired"],
+      default: "active",
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Start date must be in the past",
+      },
+    },
+    renewalDate: {
+      type: Date,
+      validate: {
+        validator: function (value) {
+          // "this" keyword refers to our model instance (arrow functions might not work with .this so we wrapped in a Function Declaration)
+          return value >= this.startDate;
+        },
+        message: "Renewal date must be after the start date",
+      },
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+subscriptionSchema.pre("save", function (next) {
+  // Auto-calculate renewal date if missing
+  if (!this.renewalDate) {
+    const renewalPeriods = {
+      daily: 1,
+      weekly: 7,
+      monthly: 30,
+      yearly: 365,
+    };
+
+    this.renewalDate = new Date(this.startDate);
+    this.renewalDate.setDate(
+      this.renewalDate.getDate() + renewalPeriods[this.frequency]
+    );
+  }
+
+  // Auto update status if renewal date has passed
+  if (this.renewalDate < new Date()) {
+    this.status = "expired";
+  }
+
+  next();
+});
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export default Subscription;
